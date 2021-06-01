154c9310d75d2a69ca93f8dea72b191ed8009469
0verchargedBattery <70082865+CrazyMan390@users.noreply.github.com>
2021-06-01 11:08:49 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+var mod = Vars.mods.locateMod("synth_tech");
+mod.meta.displayName = "[#D4F0FC]Synth [#02A9F7]Tech[#D4F0FC]nologies";
+mod.meta.description = "[blue]A small yet advanced corporation, from the Andromeda Galaxy";
+mod.meta.author = "[white]0verchargedBattery";
