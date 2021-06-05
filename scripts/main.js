ef2d465a1309fadfe6827a63d28bce151a7cb43c
0verchargedBattery <70082865+CrazyMan390@users.noreply.github.com>
2021-06-05 00:34:12 -0400

    Revert

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-const autoUpdate = require("autoupdate");
-autoUpdate.autoUpdate("synth_tech", "CrazyMan390/synth_tech", "main")
-
 var mod = Vars.mods.locateMod("synth_tech");
 mod.meta.displayName = "[#D4F0FC]Synth [#4E7CA0]Tech[#D4F0FC]nologies";
 mod.meta.description = "A small yet advanced corporation, from the Andromeda Galaxy";
